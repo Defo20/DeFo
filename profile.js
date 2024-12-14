da6490095218a63e9642e56823d9516ba601e0bb
Defo20 <ortakproje20@gmail.com>
2024-12-14 16:30:39 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Airtable bilgileriniz
-    const airtableApiKey = "pat81c3ZkZKYh8ToY"; // Airtable Personal Access Token
+    const airtableApiKey = "patBeZrQ0Q0au4viK.8b7adc0991a3686a608b08f01e7980d489def9bf813569f1eda39a7405b30a44"; // Airtable Personal Access Token
     const baseId = "appGioPVG9j8tP0dX"; // Airtable Base ID
     const tableName = "Profil"; // Airtable'daki tablo adınız
 
